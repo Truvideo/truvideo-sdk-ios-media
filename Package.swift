66924e5e966fe225a03bcccb19865ac05d4afa91
J. Doe (https://devcenter.bitrise.io/builds/setting-your-git-credentials-on-build-machines/) <please-set-your-email@bitrise.io>
2023-10-06 17:24:07 +0000

    Update 0.0.1

--- Package.swift
+++ Package.swift
@@ -1,26 +1,32 @@
 // swift-tools-version: 5.8
-// The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "TruvideoSdkMedia",
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "TruvideoSdkMedia",
-            targets: ["TruvideoSdkMedia"]),
+            targets: ["TruvideoSdkMediaTargets"]),
     ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+        .package(url: "https://github.com/aws-amplify/aws-sdk-ios-spm", exact: "2.33.4")
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .target(
+        .binaryTarget(
             name: "TruvideoSdkMedia",
-            dependencies: [],
+            url: "https://github.com/Truvideo/truvideo-sdk-ios-media/releases/download/0.0.1/TruvideoSdkMedia.xcframework.zip",
+            checksum: "3825d76bfcdd339e5fe82fe31d216d9e27add4ba6ddd3e1ec8b11948444ea1eb"
+        ),
+        .target(
+            name: "TruvideoSdkMediaTargets",
+            dependencies: [
+                .target(name: "TruvideoSdkMedia"),
+                .product(name: "AWSS3", package: "aws-sdk-ios-spm"),
+                .product(name: "AWSCore", package: "aws-sdk-ios-spm"),
+                .product(name: "AWSCognitoIdentityProvider", package: "aws-sdk-ios-spm"),
+                .product(name: "AWSCognitoIdentityProviderASF", package: "aws-sdk-ios-spm")
+            ],
             path: "Sources"
         )
     ]
