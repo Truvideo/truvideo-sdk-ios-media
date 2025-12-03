a06b9f9505846cf7447cff2c528e1837ee75313f
Bitrise CI <noreply@bitrise.io>
2025-12-03 16:29:43 +0000

    Update 78.1.2-BETA.77

--- Package.swift
+++ Package.swift
@@ -15,8 +15,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "TruvideoSdkMedia",
-            url: "https://github.com/Truvideo/truvideo-sdk-ios-media/releases/download/78.1.3/TruvideoSdkMedia.xcframework.zip",
-            checksum: "35b86c48c965b492aa6b9047721985aa530b50f417eeadc090201dcb05d1a5f9"
+            url: "https://github.com/Truvideo/truvideo-sdk-ios-media/releases/download/78.1.2-BETA.77/TruvideoSdkMedia.xcframework.zip",
+            checksum: "a86e755553961c93057c3f08a2220b0bae1093c7ceaa9fcde9108d1f90a7b00d"
         ),
         .target(
             name: "TruvideoSdkMediaTargets",
